ee4773bac7f662522e8080eb6224828b31cb3d18
Hermes Viera Amado <vieraamadoha@gmail.com>
2021-09-29 18:54:04 -0300

    event

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,28 +1,23 @@
 import { useState } from "react";
 
-const ItemCount = ({props})=>{
+const ItemCount = ({props, onConfirm})=>{
     const [count, setCount] = useState(0)
      
     const Restar = ()=>{
-        if (count === 0){
-        console.log('nada');
-    }else{
-        setCount(count -1);
-    }};
-    
+        if (count > 0){
+            setCount(count -1)
+        }}; 
     const Sumar = ()=>{
-        if(count === props.cantidad){
-            console.log('nada')
-        }else{
+        if(count < props.cantidad){ 
             setCount(count +1)
         }
-    }
-
+    } 
     return(
         <div>
             <div>{count}</div>
             <button onClick= {Restar}>-</button>
             <button onClick= {Sumar}>+</button>
+            <button onClick={()=> onConfirm(count)}>agregar al carrito</button>
         </div>
     )
 }
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,6 +1,19 @@
-import ItemCount from "../ItemCount/ItemCount"
+import ItemCount from "../ItemCount/ItemCount";
+import {Link} from "react-router-dom"
+import { useState } from "react";
 
 const ItemDetail= ({item})=>{
+        const [quantity, setQuantity]= useState(0);
+
+        const agregarAlCarrito=(numberOfProduct)=>{
+            setQuantity(numberOfProduct)
+            console("Producto Agregado")
+        }
+        if(!item){
+            return(
+                <h1>Cargando...</h1>
+            )
+        }
     return(
         <div className="container w-100 p-3">
             <div className="card mb-3">
@@ -12,7 +25,8 @@ const ItemDetail= ({item})=>{
                         <div className="card-body">
                         <h5 className="card-title">{item?.nombre}</h5>
                             <p className="card-text">{item.descripcion}</p>
-                            <ItemCount props={item}/>
+                            <ItemCount props={item} onConfirm={agregarAlCarrito}/>
+                            <Link to={'/cart'} >Ir al carrito</Link>
                         </div>
                     </div>
                 </div>
